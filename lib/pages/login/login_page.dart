5c97c0c68b4d1e9d8e27ffd9d0ea11d143c6d70f
Felipe Siqueira <flipggs@gmail.com>
2024-04-23 21:59:01 -0300

    feat: include logo

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:invelop/theme/invelop_colors.dart';
+import 'package:invelop/widgets/logo/logo_widget.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,37 +8,44 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('InVelop'),
-        ),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                  ),
-                ),
-                const SizedBox(height: 16),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                  ),
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/home');
-                  },
-                  child: const Text('Login'),
-                ),
-              ],
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const LogoWidget(),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
             ),
-          ),
-        ));
+            const SizedBox(height: 16),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            const SizedBox(height: 16),
+            Center(
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    style: ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(InVelopColors.secondary)),
+                    child: const Text('Login', style: TextStyle(color: Color(0xffffffff))),
+                  ),
+                  const SizedBox(height: 24),
+                  const Text("Esqueci a senha")
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
--- lib/theme/invelop_colors.dart
+++ lib/theme/invelop_colors.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class InVelopColors {
   static Color get background => const Color(0XFFFFFFFF);
-  static Color get secondary => const Color(0XFFB084CC);
-  static Color get primary => const Color(0XFF665687);
+  static Color get secondary => const Color(0XFF062f39);
+  static Color get primary => const Color(0XFF179F28);
   static Color get error => const Color(0XFFFF0000);
   static Color get text => const Color(0XFF000000);
 }
