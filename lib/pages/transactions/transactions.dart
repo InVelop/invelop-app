5dfbc882f8660b4a5496026dfdf7d8004bf6545e
betocostadev <robertomarx@gmail.com>
2024-05-13 19:14:57 -0300

    Improved transactions

--- lib/pages/transactions/transactions.dart
+++ lib/pages/transactions/transactions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:invelop/models/transaction_model.dart';
 import 'package:invelop/models/user_model.dart';
 import 'package:invelop/pages/account/create_account_page.dart';
@@ -19,6 +20,14 @@ class TransactionsPage extends StatefulWidget {
 }
 
 class _TransactionsPageState extends State<TransactionsPage> {
+  final _form = GlobalKey<FormState>();
+  final _accountName = TextEditingController();
+  final _amount = TextEditingController();
+  final _category = TextEditingController();
+  final _date = TextEditingController();
+  final _name = TextEditingController();
+  final _type = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final userProvider = Provider.of<UserProvider>(context, listen: false);
@@ -33,10 +42,19 @@ class _TransactionsPageState extends State<TransactionsPage> {
       transactions.toList().sort((a, b) => b.date.compareTo(a.date));
     }
 
-    print(uid);
-    print(email);
-    print(accounts);
-    print(transactions);
+    addTransaction() async {
+      print("Adding transaction");
+      print("Account: ${_accountName.text}");
+      print("Amount: ${_amount.text}");
+      print("Category: ${_category.text}");
+      print("Date: ${_date.text}");
+      print("Name: ${_name.text}");
+      print("Type: ${_type.text}");
+
+      if (_form.currentState!.validate()) {
+        print("Validated");
+      }
+    }
 
     if (accounts == null || accounts.isEmpty) {
       return const NoAccountsWidget();
@@ -144,76 +162,120 @@ class _TransactionsPageState extends State<TransactionsPage> {
                     return Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: SingleChildScrollView(
-                        child: Column(
-                          children: <Widget>[
-                            InputDecorator(
-                              decoration: const InputDecoration(
-                                labelText: "Conta",
-                                labelStyle:
-                                    TextStyle(color: InVelopColors.text),
-                              ),
-                              child: DropdownButtonHideUnderline(
-                                child: DropdownButton<String>(
-                                  items: accounts?.map((account) {
-                                    return DropdownMenuItem<String>(
-                                      value: account.name,
-                                      child: Text(account.name),
-                                    );
-                                  }).toList(),
-                                  onChanged: (_) {},
+                        child: Form(
+                          key: _form,
+                          child: Column(
+                            children: <Widget>[
+                              InputDecorator(
+                                decoration: const InputDecoration(
+                                  labelText: "Conta",
+                                  labelStyle:
+                                      TextStyle(color: InVelopColors.text),
+                                ),
+                                child: DropdownButtonHideUnderline(
+                                  child: DropdownButton<String>(
+                                    value: _accountName.text.isEmpty
+                                        ? null
+                                        : _accountName.text,
+                                    items: accounts?.map((account) {
+                                      return DropdownMenuItem<String>(
+                                        value: account.name,
+                                        child: Text(account.name),
+                                      );
+                                    }).toList(),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _accountName.text = value!;
+                                      });
+                                    },
+                                  ),
                                 ),
                               ),
-                            ),
-                            const TextField(
-                              decoration: InputDecoration(labelText: 'Valor'),
-                              keyboardType: TextInputType.number,
-                            ),
-                            const TextField(
-                              decoration:
-                                  InputDecoration(labelText: 'Category'),
-                            ),
-                            TextField(
-                              readOnly: true,
-                              decoration: const InputDecoration(
-                                labelText: 'Data',
-                                labelStyle:
-                                    TextStyle(color: InVelopColors.text),
+                              TextFormField(
+                                controller: _amount,
+                                style:
+                                    const TextStyle(color: InVelopColors.text),
+                                decoration: const InputDecoration(
+                                  labelText: 'Valor',
+                                  labelStyle:
+                                      TextStyle(color: InVelopColors.text),
+                                ),
+                                keyboardType: TextInputType.number,
                               ),
-                              onTap: () async {
-                                var date = await showDatePicker(
-                                  context: context,
-                                  initialDate: DateTime.now(),
-                                  firstDate: DateTime(2000),
-                                  lastDate: DateTime(2100),
-                                );
-                                if (date != null) {
-                                  // Do something with the selected date
-                                }
-                              },
-                            ),
-                            const TextField(
-                              decoration: InputDecoration(labelText: 'Nome'),
-                            ),
-                            InputDecorator(
-                              decoration: const InputDecoration(
-                                labelText: "Tipo de transação",
-                                labelStyle:
-                                    TextStyle(color: InVelopColors.text),
-                              ),
-                              child: DropdownButtonHideUnderline(
-                                child: DropdownButton<String>(
-                                  items: <String>['Income', 'Outcome']
-                                      .map((String value) {
-                                    return DropdownMenuItem<String>(
-                                      value: value,
-                                      child: Text(value),
-                                    );
-                                  }).toList(),
-                                  onChanged: (_) {},
+                              TextFormField(
+                                controller: _category,
+                                style:
+                                    const TextStyle(color: InVelopColors.text),
+                                decoration: const InputDecoration(
+                                  labelText: 'Categoria',
+                                  labelStyle:
+                                      TextStyle(color: InVelopColors.text),
                                 ),
                               ),
-                            ),
-                          ],
+                              TextFormField(
+                                controller: _date,
+                                readOnly: true,
+                                style:
+                                    const TextStyle(color: InVelopColors.text),
+                                decoration: const InputDecoration(
+                                  labelText: 'Data',
+                                  labelStyle:
+                                      TextStyle(color: InVelopColors.text),
+                                ),
+                                onTap: () async {
+                                  var date = await showDatePicker(
+                                    context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime(2000),
+                                    lastDate: DateTime(2100),
+                                  );
+                                  if (date != null) {
+                                    _date.text =
+                                        DateFormat('dd-MM-yyyy').format(date);
+                                  }
+                                },
+                              ),
+                              TextFormField(
+                                controller: _name,
+                                style:
+                                    const TextStyle(color: InVelopColors.text),
+                                decoration: const InputDecoration(
+                                  labelText: 'Nome',
+                                  labelStyle:
+                                      TextStyle(color: InVelopColors.text),
+                                ),
+                              ),
+                              InputDecorator(
+                                decoration: const InputDecoration(
+                                  labelText: "Tipo",
+                                  labelStyle:
+                                      TextStyle(color: InVelopColors.text),
+                                ),
+                                child: DropdownButtonHideUnderline(
+                                  child: DropdownButton<String>(
+                                    items: <String>['Income', 'Outcome']
+                                        .map((String value) {
+                                      return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(value),
+                                      );
+                                    }).toList(),
+                                    onChanged: (value) {
+                                      _type.text = value!;
+                                    },
+                                  ),
+                                ),
+                              ),
+                              TextButton(
+                                child: const Text('Add Transaction',
+                                    style:
+                                        TextStyle(color: InVelopColors.text)),
+                                onPressed: () {
+                                  addTransaction();
+                                },
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     );
